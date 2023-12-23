0ee6610d61286f381f6beb3eccc27003f0c0a3ee
Dimmagi0 <redscool@mail.ru>
2023-12-23 23:28:08 +0300

    Added DZ_6

---
+++ Z_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+
+// Пример a b c => “abcdef” 
+//        d e f
+
+
+char[,] elements = new char[,] 
+{
+    {'a', 'b', 'c'},
+    {'d', 'e', 'f'}
+};
+
+string str = "";
+
+for (int i = 0; i < elements.GetLength(0); i++)
+{
+    for (int j = 0; j < elements.GetLength(1); j++)
+    {
+       str += elements[i,j]; 
+    }
+}
+Console.WriteLine(str);
---
+++ Z_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 3: Задайте произвольную строку. 
+// Выясните, является ли она палиндромом.
+// “aBcD1ef!-” => Нет
+// “шалаш” => Да
+// “55655” => Да
+
+Console.WriteLine("Введите строку: ");
+string text = Console.ReadLine();
+
+if (IsPalindrom(text))
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
+
+static bool IsPalindrom(string word)
+{
+  var left = 0;
+  var right = word.Length -1;
+58
+  while (left<right)
+  {
+    if (word[left] != word[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
